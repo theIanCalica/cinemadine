95da6976039156b9f9e44e9516ecface7170e940
Ian Calica <Iggc654@gmail.com>
2024-08-23 21:53:28 +0800

    uodated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
@@ -6,12 +8,16 @@ import "@fontsource/roboto/700.css";
 import { CssBaseline } from "@mui/material";
 import SigninPage from "./pages/SigninPage";
 import AdminIndexPage from "./pages/admin/AdminIndexPage";
+
 function App() {
   return (
-    <>
+    <Router>
       <CssBaseline />
-      <AdminIndexPage />
-    </>
+      <Routes>
+        <Route path="/" element={<SigninPage />} />
+        <Route path="/admin" element={<AdminIndexPage />} />
+      </Routes>
+    </Router>
   );
 }
 
