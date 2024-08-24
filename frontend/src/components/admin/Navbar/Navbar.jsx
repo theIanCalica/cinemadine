95256a9dfd0b5516f2cf689a275375d0d51f16f4
Ian Calica <Iggc654@gmail.com>
2024-08-24 23:03:42 +0800

    updated

--- frontend/src/components/admin/Navbar/Navbar.jsx
+++ frontend/src/components/admin/Navbar/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 const Navbar = () => {
   return (
     <div className="navbar">
@@ -18,6 +23,21 @@ const Navbar = () => {
           <div className="item">
             <LanguageOutlinedIcon />
             English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon />
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon />
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
           </div>
         </div>
       </div>
